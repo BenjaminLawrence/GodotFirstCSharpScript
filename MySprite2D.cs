a7eb13ab3e449ecfd4ddcb1787a12c84aab1da92
BenjaminLawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2024-04-27 20:51:05 +0800

    Added motion button and timer

--- MySprite2D.cs
+++ MySprite2D.cs
@@ -7,16 +7,24 @@ public partial class MySprite2D : Sprite2D
 	
 	public override void _Process(double delta)
 	{
-		var direction = 0;
-		if (Input.IsActionPressed("ui_left"))
-			direction = -1;
-		if (Input.IsActionPressed("ui_right"))
-			direction = 1;
-		Rotation += _angularSpeed * direction * (float)delta;
-		
-		var velocity = Vector2.Zero;
-		if (Input.IsActionPressed("ui_up"))
-			velocity = Vector2.Up.Rotated(Rotation) * _speed;
+		Rotation += _angularSpeed * (float)delta;
+		var velocity = Vector2.Up.Rotated(Rotation) * _speed;
 		Position += velocity * (float)delta;
+	}
+	
+	private void OnButtonPressed()
+	{
+		SetProcess(!IsProcessing());
+	}
+	
+	public override void _Ready()
+	{
+		var timer = GetNode<Timer>("Timer");
+		timer.Timeout += OnTimerTimeout;
+	}
+	
+	private void OnTimerTimeout()
+	{
+		Visible = !Visible;
 	}
 }
